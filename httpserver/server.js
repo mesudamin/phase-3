c6ba0850ce68333bf7304ee1087af8d4767f2654
mesudamin <lalemasud@gmail.com>
2026-05-24 22:24:38 +0300

    adding stutes

--- httpserver/server.js
+++ httpserver/server.js
@@ -1,15 +1,17 @@
 let http = require("http");
 let server = http.createServer((req, res) => {
   if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("hello world");
   } else if (req.url === "/about") {
     res.end("about page");
   } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("404 not found");
   }
 });
 
-port = 3000;
+const port = 3000;
 server.listen(port, () => {
   console.log("serveris running on port 3000");
 });
